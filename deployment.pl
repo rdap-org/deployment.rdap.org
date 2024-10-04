9161329e212c3783e9c6d67b9286c768cd45d4b4
Gavin Brown <gavin.brown@fastmail.uk>
2024-10-04 17:26:50 +0100

    * upsert wasn't working, so do separate SQL statements for insert and update * delete TLDs that have gone away * highlight TLDs with stealth RDAP

--- deployment.pl
+++ deployment.pl
@@ -9,6 +9,7 @@ use File::Spec;
 use HTML::Tiny;
 use IPC::Open2;
 use JSON::XS;
+use List::Util qw(any none);
 use Net::DNS::SEC;
 use Net::RDAP;
 use Number::Format qw(:subs);
@@ -23,6 +24,7 @@ use constant {
     STATDNS_STATS_URL       => 'https://importhtml.site/csv/?url=https%3A//www.statdns.com/',
     NTLDSTATS_STATS_URL     => 'https://importhtml.site/csv/?url=https%3A//ntldstats.com/tld/',
     PAGE_TEMPLATE           => 'https://raw.githubusercontent.com/rdap-org/about.rdap.org/main/_layouts/page.html',
+    STEALTH_LIST            => 'https://etc.gavinbrown.xyz/stealth.dat',
 };
 use utf8;
 use feature qw(say);
@@ -54,6 +56,10 @@ $out->close;
 waitpid($pid, 0);
 
 say STDERR 'generated U-label mapping';
+
+my %stealth = map { lc($_) => 1 } grep { length > 0 } (split(/\n/, mirror_str(STEALTH_LIST)));
+
+say STDERR 'retrieved stealth server list';
 
 my %enabled;
 my $registry = Net::RDAP::Registry::IANARegistry->new(mirror_json(RDAP_BOOTSTRAP_URL));
@@ -140,16 +146,37 @@ $db->do(q{
     )
 });
 
-my $sth = $db->prepare(q{
-    INSERT INTO rdap_deployment_report
+my $isth = $db->prepare(q{
+    INSERT INTO `rdap_deployment_report`
     (`tld`, `type`, `rdap`, `https`, `dnssec`, `dane`, `dums`, `rdap_enabled_on`)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    ON CONFLICT(`tld`) DO UPDATE
-    SET `type`=?, `rdap`=?, `https`=?, `dnssec`=?, `dane`=?, `dums`=?,
-    `rdap_enabled_on`=COALESCE(`rdap_enabled_on`, ?)
 });
 
-say STDERR 'connected to database, updating records...';
+my $usth = $db->prepare(q{
+    UPDATE `rdap_deployment_report`
+    SET `type`=?, `rdap`=?, `https`=?, `dnssec`=?, `dane`=?, `dums`=?, `rdap_enabled_on`=?
+    WHERE (`tld`=?)
+});
+
+say STDERR 'connected to database';
+
+my $ssth = $db->prepare(q{SELECT * FROM `rdap_deployment_report`});
+$ssth->execute;
+
+my $dsth = $db->prepare(q{DELETE FROM `rdap_deployment_report` WHERE (`tld`=?)});
+
+my %records;
+while (my $row = $ssth->fetchrow_hashref) {
+    $row = anon($row);
+    if (none { $_ eq $row->tld } @tlds) {
+        say STDERR sprintf('.%s no longer exists, deleting', $row->tld);
+        $dsth->execute($row->tld);
+
+    } else {
+        $records{$row->tld} = $row;
+
+    }
+}
 
 my $resolver = Net::DNS::Resolver->new(
     'nameservers'       => [qw(1.1.1.1)],
@@ -165,11 +192,9 @@ foreach my $tld (@tlds) {
 	my $rdap    = exists($enabled{$tld});
 	my $https   = $rdap && (0 < scalar(grep { 'https' eq $_->scheme } @{$enabled{$tld}}));
 
-	my ($dnssec, $dane, $date);
+	my ($dnssec, $dane, $rdap_enabled_on);
 
     if ($rdap) {
-        $date = $TODAY;
-
     	URL: foreach my $url (@{$enabled{$tld}}) {
             my $host = lc($url->host);
 
@@ -191,13 +216,46 @@ foreach my $tld (@tlds) {
 
             last URL if ($dnssec && $dane);
     	}
+
+        $rdap_enabled_on = $records{$tld}->rdap_enabled_on || $TODAY;
+
+    } else {
+        $rdap_enabled_on = undef;
+
     }
 
-    my @values = ($type{$tld}, map { int } ($rdap, $https, $dnssec, $dane, $dums{$tld}), $date);
+    if (!exists($records{$tld})) {
+        my @values = (
+            $tld,
+            $type{$tld},
+            $rdap,
+            $https,
+            $dnssec,
+            $dane,
+            $dums{$tld},
+            $rdap_enabled_on,
+        );
 
-    $sth->execute($tld, @values, @values);
+        $isth->execute(@values);
 
-    say STDERR sprintf('updated .%s', $tld);
+        say STDERR sprintf('inserted new record for .%s', $tld);
+
+    } else {
+        my @values = (
+            $type{$tld},
+            $rdap,
+            $https,
+            $dnssec,
+            $dane,
+            $dums{$tld},
+            $date,
+            $rdap_enabled_on,
+        );
+
+        $usth->execute(@values);
+
+        say STDERR sprintf('updated .%s', $tld);
+    }
 }
 
 say STDERR 'generating HTML';
@@ -263,17 +321,19 @@ foreach my $ref (sort { $b->{'dums'} <=> $a->{'dums'} } @rows) {
         $stats->{$stats_type} = [['Deployment Status', 'Approx # Domains'], ['Not available', 0], ['Available', 0]];
     }
 
+    my $stealth = exists($stealth{$row->tld});
+
     $stats->{'all'}->[1+$row->rdap]->[1] += $row->dums;
     $stats->{$stats_type}->[1+$row->rdap]->[1] += $row->dums;
 
     $content .= $h->td({class => 'text-center tld', title => '.'.$row->tld}, '.'.idn_to_unicode($row->tld));
     $content .= $h->td({class => 'text-center type'}, $row->type);
     $content .= $h->td({class => 'dums', style => 'text-align:right'}, format_number($row->dums));
-    $content .= $h->td({class => 'text-center text-'.($row->rdap ? 'success' : 'danger')}, $row->rdap ? 'Yes' : 'No');
-    $content .= $h->td({class => 'text-center text-'.($row->rdap ? 'success' : 'danger')}, $row->rdap_enabled_on || '-');
-    $content .= $h->td({class => 'text-center text-'.($row->https ? 'success' : 'danger')}, $row->https ? 'Yes' : 'No');
+    $content .= $h->td({class => 'text-center text-'.($row->rdap   ? 'success' : ($stealth ? 'warning' : 'danger'))}, $row->rdap ? 'Yes' : ($stealth ? 'No*' : 'No'));
+    $content .= $h->td({class => 'text-center text-'.($row->rdap   ? 'success' : 'danger')}, $row->rdap_enabled_on || '-');
+    $content .= $h->td({class => 'text-center text-'.($row->https  ? 'success' : 'danger')}, $row->https ? 'Yes' : 'No');
     $content .= $h->td({class => 'text-center text-'.($row->dnssec ? 'success' : 'danger')}, $row->dnssec ? 'Yes' : 'No');
-    $content .= $h->td({class => 'text-center text-'.($row->dane ? 'success' : 'danger')}, $row->dane ? 'Yes' : 'No');
+    $content .= $h->td({class => 'text-center text-'.($row->dane   ? 'success' : 'danger')}, $row->dane ? 'Yes' : 'No');
 
     if ('country-code' eq $row->type) {
         my $cc = uc('uk' eq $row->tld ? 'gb' : $row->tld);
@@ -285,6 +345,14 @@ foreach my $ref (sort { $b->{'dums'} <=> $a->{'dums'} } @rows) {
 
 $content .= $h->close('tbody');
 $content .= $h->close('table');
+
+if (scalar(keys(%stealth)) > 0) {
+    $content .= $h->p($h->em(
+        '* This TLD has a '
+        .$h->a({target => '_new', href => 'https://regiops.net/row13-agenda#:~:text=15:00,Stealth%20RDAP'}, 'Stealth RDAP')
+        .' Server.'
+    ));
+}
 
 my $json = JSON::XS->new->utf8;
 
